e7493192102ed39a6ccac5adc1b604c66cd37c5d
Alexandr <saint12maloj@gmail.com>
2020-02-08 23:42:13 +0300

    Swagger added (+ xml annotation is set up)

--- DemoCoreAPIWeb/Startup.cs
+++ DemoCoreAPIWeb/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace DemoCoreAPIWeb
 {
@@ -26,6 +29,15 @@ namespace DemoCoreAPIWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(setup => {
+                setup.SwaggerDoc("v1", new OpenApiInfo { Version = "v1", Title = "DemoCoreApiWebApplication" });
+
+                // These 3 lines set the XML comments for Swagger (don't forget add some lines of the PropertyGroup in .csproj !!!).
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                setup.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +53,12 @@ namespace DemoCoreAPIWeb
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(setup =>
+            {
+                setup.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger for Demo Core API by Kalyuganov");                
+            });
 
             app.UseEndpoints(endpoints =>
             {
