8a10aa452f96dd63a4ada670bdf136fe851894cf
Alexandr <saint12maloj@gmail.com>
2020-02-14 01:48:38 +0300

    Additional migration (UserModel changed)

---
+++ DemoCoreAPI.Data.SQLServer/Migrations/20200213224721_DbChangedAgeAdded.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DemoCoreAPI.Data.SQLServer.Migrations
+{
+    public partial class DbChangedAgeAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Age",
+                value: 29);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Users");
+        }
+    }
+}
