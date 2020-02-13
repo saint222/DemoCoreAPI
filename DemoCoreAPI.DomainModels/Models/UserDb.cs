6dc9c296d6346341556982b0087fe0302fc7eeeb
Alexandr <saint12maloj@gmail.com>
2020-02-13 23:29:26 +0300

    UserModel added

---
+++ DemoCoreAPI.DomainModels/Models/UserDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoCoreAPI.DomainModels.Models
+{
+    public class UserDb
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; } = false;
+    }
+}
