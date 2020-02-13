e49b7dabdd6308a658d37a47de334dd7b492e0ab
Alexandr <saint12maloj@gmail.com>
2020-02-14 00:53:49 +0300

    ViewModels, Context, Connections string added

---
+++ DemoCoreAPI.BusinessLogic/ViewModels/LoginResultViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoCoreAPI.BusinessLogic.ViewModels
+{
+    public class LoginResultViewModel
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ DemoCoreAPI.Data.SQLServer/MainContext.cs
@@ -0,0 +1,30 @@
+﻿using DemoCoreAPI.DomainModels.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoCoreAPI.Data.SQLServer
+{
+    public class MainContext : DbContext
+    {
+        public MainContext(DbContextOptions options) : base(options) { }
+        
+        public DbSet<UserDb> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserDb>().HasData(
+                new UserDb
+                {
+                    Id = 1,
+                    FirstName = "Aleksandr",
+                    LastName = "Kalyuganov",
+                    Email = "saint12maloj@gmail.com",
+                    Password = "qqLQK/L5n5GqeiaCEkxVrUxlkbAWMmPUlOBSmlGXnPA=",
+                    IsAdmin = true
+                });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
