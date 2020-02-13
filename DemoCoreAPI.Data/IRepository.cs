944b1d0d3977a9e8434c1b8d07ca7a37441e1a55
Alexandr <saint12maloj@gmail.com>
2020-02-13 23:29:55 +0300

    Irepository added

---
+++ DemoCoreAPI.Data/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DemoCoreAPI.Data
+{
+    public interface IRepository<T> where T: class 
+    {        
+        IQueryable<T> GeAll();
+        IQueryable<T> Where (Expression<Func<T, bool>> predicate);
+        void Add(T entry);
+        void Update(T entry);
+        void Remove(T entity);
+        int SaveChanges();
+    }
+}
