e53ecade4743c8800c6f5f89e2d9356882a8b0ec
Alexandr <saint12maloj@gmail.com>
2020-03-07 00:34:58 +0300

    Repository changed

--- DemoCoreAPI.Data/IRepository.cs
+++ DemoCoreAPI.Data/IRepository.cs
@@ -7,7 +7,8 @@ using System.Text;
 namespace DemoCoreAPI.Data
 {
     public interface IRepository<T> where T: class 
-    {        
+    { 
+        T FindById(long id);
         IQueryable<T> GeAll();
         IQueryable<T> Where (Expression<Func<T, bool>> predicate);
         void Add(T entry);
