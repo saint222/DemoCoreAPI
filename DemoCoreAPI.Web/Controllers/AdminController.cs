31e045da920f604b8265bd8030c29fe3bb196ce4
Alexandr <saint12maloj@gmail.com>
2020-06-20 01:10:45 +0300

    Got rid of try/catch blocks in AdminController

--- DemoCoreAPI.Web/Controllers/AdminController.cs
+++ DemoCoreAPI.Web/Controllers/AdminController.cs
@@ -33,24 +33,16 @@ namespace DemoCoreAPI.Web.Controllers
         [Route("Users")]
         public IActionResult GetUsers()
         {
-            try
-            {
-                var users = _adminService.GetAllUsers();
-                if (users.Count() == 0)
-                    return NotFound();
-                return Ok(users);
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Exception occured while retrieving data.");
-                return ServerError(ex);
-            }
+            var users = _adminService.GetAllUsers();
+            if (users.Count() == 0)
+                return NotFound();
+            return Ok(users);
         }
 
         [HttpGet]
         [Route("GenerateExcel")]
         public IActionResult Excel()
-        {           
+        {
             using (var stream = new MemoryStream())
             {
                 var users = _adminService.GetAllUsers();
@@ -59,124 +51,42 @@ namespace DemoCoreAPI.Web.Controllers
                 var content = stream.ToArray();
 
                 return File(
-                            content, 
+                            content,
                             "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                             "Users.xlsx"
                             );
             }
         }
-       
+
         [HttpGet]
         [Route("User/{id}")]
         public IActionResult GetUserById(long id)
         {
-            try
-            {
-                var user = _adminService.GetUserById(id);
-                return Ok(user);
-            }
-            catch (ArgumentException ex)
-            {
-                Log.Error(ex, $"Id {id} is invalid.");
-                return BadRequest(ex);
-            }
-            catch (NotFoundException ex)
-            {
-                Log.Error(ex, $"User with Id {id} doesn't exist.");
-                return BadRequest(ex);
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Exception occured while retrieving data.");
-                return ServerError(ex);
-            }
+            var user = _adminService.GetUserById(id);
+            return Ok(user);
         }
 
         [HttpPost]
         [Route("Add")]
         public IActionResult AddNewUser(NewUserBindingModel model)
         {
-            try
-            {
-                var newUser = _adminService.CreateNewUser(model);
-                return Ok(newUser);
-            }
-            catch (ArgumentNullException ex)
-            {
-                Log.Error(ex, $"Model {model} is null");
-                return BadRequest(ex);
-            }
-            catch (ArgumentException ex)
-            {
-                Log.Error(ex, $"The fields of the model {model} are null");
-                return BadRequest(ex);
-            }
-            catch (EmailDuplicateException ex)
-            {
-                Log.Error(ex, "User with such email already exists.");
-                return BadRequest(ex); ;
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Exception occured while creating a new user.");
-                return ServerError(ex);
-            }
+            var newUser = _adminService.CreateNewUser(model);
+            return Ok(newUser);
         }
 
         [HttpPut]
         [Route("Update")]
         public IActionResult UpdateUser(UpdateUserBindingModel model)
         {
-            try
-            {
-                var updateUser = _adminService.UpdateUser(model);
-                return Ok(updateUser);
-            }
-            catch (ArgumentNullException ex)
-            {
-                Log.Error(ex, $"Model {model} is null");
-                return BadRequest(ex);
-            }
-            catch (NotFoundException ex)
-            {
-                Log.Error(ex, $"User {model} is not found.");
-                return BadRequest(ex);
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Exception occured while updating user.");
-                return ServerError(ex);
-            }
+            var updateUser = _adminService.UpdateUser(model);
+            return Ok(updateUser);
         }
         [HttpDelete]
         [Route("Delete")]
         public IActionResult DeleteUser(long id)
         {
-            try
-            {
-                var result = _adminService.RemoveUser(id);
-                return Ok(result);
-            }
-            catch (ArgumentException ex)
-            {
-                Log.Error(ex, $"Id {id} is invalid.");
-                return BadRequest(ex);
-            }
-            catch (NotFoundException ex)
-            {
-                Log.Error(ex, $"User with Id {id} doesn't exist.");
-                return BadRequest(ex);
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Exception occured while deleting user.");
-                return ServerError(ex);
-            }
-        }
-
-        private IActionResult ServerError(Exception ex)
-        {
-            return Json(new { Message = "Internal server error."}, ex);
+            var result = _adminService.RemoveUser(id);
+            return Ok(result);
         }
 
         private XLWorkbook GenerateXLDoc(ICollection<UserAPIModel> users)
