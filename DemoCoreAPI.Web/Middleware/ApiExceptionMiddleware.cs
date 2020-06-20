7e73f6174a44b7695e004216a5c8e2f0e97f3159
Alexandr <saint12maloj@gmail.com>
2020-06-21 00:25:44 +0300

    ExceptionMiddleware refactoring

--- DemoCoreAPI.Web/Middleware/ApiExceptionMiddleware.cs
+++ DemoCoreAPI.Web/Middleware/ApiExceptionMiddleware.cs
@@ -35,85 +35,28 @@ namespace DemoCoreAPI.Web.Middleware
             {
                 await _next(context);
             }
-            catch (ArgumentNullException ex)
-            {
-                Log.Error(ex, "ArgumentNullException.");
-                if (context.Response.HasStarted)
-                {
-                    throw;
-                }
-
-                context.Response.StatusCode = 400;
-                context.Response.ContentType = "application/json";
-                context.Response.Headers.Add("exception", "validationException");
-                var errorJson = JsonConvert.SerializeObject(new { message = ex.Message, stackTrace = ex.StackTrace }, _jsonSettings);
-                await context.Response.WriteAsync(errorJson);
-            }
-            catch (ArgumentException ex)
-            {
-                Log.Error(ex, "ArgumentException.");
-                if (context.Response.HasStarted)
-                {
-                    throw;
-                }
-
-                context.Response.StatusCode = 400;
-                context.Response.ContentType = "application/json";
-                context.Response.Headers.Add("exception", "validationException");
-                var errorJson = JsonConvert.SerializeObject(new { message = ex.Message, stackTrace = ex.StackTrace }, _jsonSettings);
-                await context.Response.WriteAsync(errorJson);
-            }
-            catch (EmailDuplicateException ex)
-            {
-                Log.Error(ex, "EmailDuplicateException.");
-                if (context.Response.HasStarted)
-                {
-                    throw;
-                }
-
-                context.Response.StatusCode = 400;
-                context.Response.ContentType = "application/json";
-                context.Response.Headers.Add("exception", "validationException");
-                var errorJson = JsonConvert.SerializeObject(new { message = ex.Message, stackTrace = ex.StackTrace }, _jsonSettings);
-                await context.Response.WriteAsync(errorJson);
-            }
-            catch (PasswordMismatchException ex)
-            {
-                Log.Error(ex, "PasswordMismatchException.");
-                if (context.Response.HasStarted)
-                {
-                    throw;
-                }
-
-                context.Response.StatusCode = 400;
-                context.Response.ContentType = "application/json";
-                context.Response.Headers.Add("exception", "validationException");
-                var errorJson = JsonConvert.SerializeObject(new { message = ex.Message, stackTrace = ex.StackTrace }, _jsonSettings);
-                await context.Response.WriteAsync(errorJson);
-            }
-            catch (NotFoundException ex)
-            {
-                Log.Error(ex, "NotFoundException.");
-                if (context.Response.HasStarted)
-                {
-                    throw;
-                }
-
-                context.Response.StatusCode = 404;
-                context.Response.ContentType = "application/json";
-                context.Response.Headers.Add("exception", "validationException");
-                var errorJson = JsonConvert.SerializeObject(new { message = ex.Message, stackTrace = ex.StackTrace }, _jsonSettings);
-                await context.Response.WriteAsync(errorJson);
-            }
             catch (Exception ex)
             {
-                Log.Error(ex, "ServerException.");
                 if (context.Response.HasStarted)
                 {
                     throw;
                 }
-
-                context.Response.StatusCode = 500;
+                switch (ex)
+                {
+                    case ArgumentNullException _:
+                    case ArgumentException _:
+                    case EmailDuplicateException _:
+                    case PasswordMismatchException _:
+                        context.Response.StatusCode = 400;
+                        break;
+                    case NotFoundException _:
+                        context.Response.StatusCode = 404;
+                        break;
+                    default:
+                        context.Response.StatusCode = 500;
+                        break;
+                }
+                Log.Error(ex, null);
                 context.Response.ContentType = "application/json";
                 context.Response.Headers.Add("exception", "serverException");
                 var errorJson = JsonConvert.SerializeObject(new { message = ex.Message, stackTrace = ex.StackTrace }, _jsonSettings);
